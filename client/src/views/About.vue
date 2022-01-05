e4c4e56a2c7d9f58aae1c89cde4461ecfb8c624b
Oussama Filani <42185573+filani07@users.noreply.github.com>
2022-01-05 01:40:40 +0100

    init

---
+++ client/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    hide() {
+      console.log("halwa");
+      return this.$route.path === "/";
+    },
+  },
+};
+</script>
+
+
